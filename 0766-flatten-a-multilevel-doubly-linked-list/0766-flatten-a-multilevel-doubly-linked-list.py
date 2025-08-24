7bf3f3b3f9693f9af23e1fd5ef64a6e008fefa52
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2025-08-24 20:04:43 +0530

    Time: 38 ms (73.71%), Space: 18.3 MB (70.53%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.py
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.py
@@ -12,18 +12,21 @@ class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head is None:
             return None
-        def flattenDFS(prev, curr):
-            if curr is None:
-                return prev
+        
+        def flattenDFS(previousNode, currentNode):
+            if currentNode is None:
+                return previousNode
 
-            curr.prev = prev
-            prev.next = curr
-            tempNext = curr.next   # store next
-            tail = flattenDFS(curr, curr.child)  # recursively flatten child list
-            curr.child = None  # child pointer remove kar diya
+            currentNode.prev = previousNode
+            previousNode.next = currentNode
+
+            tempNext = currentNode.next   # store next
+            tail = flattenDFS(currentNode, currentNode.child)  # recursively flatten child list
+            currentNode.child = None  # child pointer remove kar diya
+
             return flattenDFS(tail, tempNext)  # continue from saved next
 
-        dummy = Node(0)
+        dummy = Node(0, None, None, None)
         flattenDFS(dummy, head)
         dummy.next.prev = None
         return dummy.next

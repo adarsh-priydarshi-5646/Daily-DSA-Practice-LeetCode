8089d2689020fcda6a7cc757c1959d8a26097894
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2025-09-14 20:43:56 +0530

    Time: 0 ms (100%), Space: 18.3 MB (95.88%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = sorted(nums)
+        leftpoint = 0
+        rightpoint = len(arr)-1
+        while leftpoint < rightpoint:
+            su_m = arr[leftpoint] + arr[rightpoint]
+            if su_m == target:
+                a,b = arr[leftpoint],arr[rightpoint]
+                break
+            if su_m < target:
+                leftpoint+=1
+            else:
+                rightpoint-=1
+        result=[]
+        if a != b:
+            result.append(nums.index(a))
+            result.append(nums.index(b))
+            return sorted(result)
+        else:
+            first_index = nums.index(a)
+            second_index = nums.index(b, first_index + 1)
+            result.append(first_index)
+            result.append(second_index)
+            return result
+            

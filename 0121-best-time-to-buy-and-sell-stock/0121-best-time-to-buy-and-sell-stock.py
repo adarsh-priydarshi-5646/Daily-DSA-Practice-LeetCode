66bc05374bb3815f884d8acea5cae992c6032d07
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2025-11-06 18:02:00 +0530

    Time: 27 ms (88.2%), Space: 27.1 MB (8.62%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Step 1: Initialize min_price as infinity (bahut bada number) & max_profit as 0
-        min_price = float('inf')  # ab tak ka sabse chhota price
-        max_profit = 0            # ab tak ka max profit
-
-        # Step 2: Loop through each price
+        min_price = float('inf')
+        max_price = 0
         for price in prices:
-            # Agar current price ab tak ka minimum hai, toh update karo
             if price < min_price:
                 min_price = price
-            # warna calculate karo profit agar hum aaj sell karte
             else:
                 profit = price - min_price
-                # agar naya profit zyada hai, toh max_profit update karo
-                if profit > max_profit:
-                    max_profit = profit
-
-        # Step 3: Return the maximum profit found
-        return max_profit
+                if profit > max_price:
+                    max_price = profit
+        return max_price
         

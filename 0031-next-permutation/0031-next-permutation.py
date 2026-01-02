ec3688f64eca1c1e0fe89f1c2f83285e473eb2d5
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-01-02 16:28:17 +0530

    Time: 0 ms (100%), Space: 17.5 MB (84.15%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -10,13 +10,13 @@ class Solution:
         if pivot == -1:
             nums.reverse()
             return
+            
         arr1 = nums[:pivot]
         arr2 = nums[pivot:]
 
-        for i in range(len(arr2) - 1, 0, -1):
-            if arr2[i] > arr2[0]:
-                arr2[0], arr2[i] = arr2[i], arr2[0]
+        for i in range(len(arr2) - 1, 0, -1): 
+            if arr2[i] > arr2[0]: 
+                arr2[0], arr2[i] = arr2[i], arr2[0] 
                 break
-
         arr2[1:] = sorted(arr2[1:])
         nums[:] = arr1 + arr2

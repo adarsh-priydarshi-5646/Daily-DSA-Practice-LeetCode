32ce4404ebfc0bccaac785c403f8fdd4f5e7a61b
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-08-20 23:07:33 +0530

    Time: 7 ms (29.15%), Space: 21.1 MB (43.5%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # maxi = max(nums)
+        # hash_nums = [0] * (maxi + 1)
+        # for num in nums:
+        #     hash_nums[num] += 1
+        # for num in nums:
+        #     if hash_nums[num] == 1:
+        #         return num
+        # return -1
+        result = 0
+        for num in nums:
+            result ^= num  # This cancels out all duplicate pairs
+        return result
+
+        # for i in range(len(nums)):
+        #     num = nums[i]
+        #     cnt = 0
+        #     for j in range(len(nums)):
+        #         if nums[j] == num:
+        #             cnt += 1
+        #     if cnt == 1:
+        #         return num
+        

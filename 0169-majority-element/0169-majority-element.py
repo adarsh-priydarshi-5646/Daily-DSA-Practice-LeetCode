79cdfa874886785214e82ece05cff6f3e0026a72
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-08-27 23:50:55 +0530

    Time: 10 ms (35.22%), Space: 21.3 MB (18.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash_map = {}
-        for i in range(len(nums)):
-            if nums[i] not in hash_map:
-                hash_map[nums[i]] = 1
+        #better
+        ele = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if count == 0:
+                ele = nums[i]
+                count += 1
+            elif nums[i] == ele:
+                count += 1
             else:
-                hash_map[nums[i]] += 1
-        for num in hash_map:
-            if hash_map[num] > len(nums)//2:
-                return num
+                count -= 1
+        return ele
+        
+
+
+
+
+        #better
+        # hash_map = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in hash_map:
+        #         hash_map[nums[i]] = 1
+        #     else:
+        #         hash_map[nums[i]] += 1
+        # for num in hash_map:
+        #     if hash_map[num] > len(nums)//2:
+        #         return num
 
         #brute force
         # for i in range(len(nums)):

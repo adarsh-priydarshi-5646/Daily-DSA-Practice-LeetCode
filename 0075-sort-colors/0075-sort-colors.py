8a753a1d33a574bea2b80dc80a1d020e84e858d0
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-08-27 23:21:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (62.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,20 +3,36 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        j = 0
-        k = len(nums) - 1
-
-        while j <= k:
-            if nums[j] == 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j += 1
-            elif nums[j] == 1:
-                j += 1
-            else:
-                nums[j], nums[k] = nums[k], nums[j]
-                k -= 1
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low +=1 
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
         return nums
 
-        
+        #brute
+        # cnt0 = 0
+        # cnt1 = 0
+        # cnt2 = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         cnt0 += 1
+        #     if nums[i] == 1:
+        #         cnt1 += 1
+        #     if nums[i] == 2:
+        #         cnt2 += 1
+        # for i in range(cnt0):
+        #     nums[i] = 0
+        # for i in range(cnt0,cnt0+cnt1):
+        #     nums[i] = 1
+        # for i in range(cnt0+cnt1, len(nums)):
+        #     nums[i] = 2
+        # return nums

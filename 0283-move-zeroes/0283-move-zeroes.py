a63de8d5bdafadcb0f2784cce7c4f70009262e1f
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-09-04 14:11:42 +0530

    Time: 5 ms (49.07%), Space: 20.6 MB (5.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,27 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Pointer for the position of the next non-zero element
-        j = 0 
-        
-        # Iterate through the array with pointer i
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                # Swap elements
-                nums[j], nums[i] = nums[i], nums[j]
-                # Move the non-zero position pointer forward
-                j += 1
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
+                i += 1
+
         return nums
 
-
-        #brute
-        # temp = []
-        # for i in range(len(nums)):
-        #     if nums[i] != 0:
-        #         temp.append(nums[i])
-        # for i in range(len(temp)):
-        #     nums[i] = temp[i]
-        # for i in range(len(temp), len(nums)):
-        #     nums[i] = 0
-        # return nums
         
+
+
+
+
+
+
+
+
+
+
+

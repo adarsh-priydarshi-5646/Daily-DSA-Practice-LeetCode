4ce9357528d7e379d8df5cfb732abfa33485d642
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-09-05 23:29:18 +0530

    Time: 5719 ms (5.02%), Space: 20.4 MB (75.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,23 +1,21 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        ss = n*(n+1)//2
-        s = 0
-        for i in range(n):
-            s += nums[i]
-        return ss - s
-
-
-
         # n = len(nums)
-        # for i in range(n+1):
-        #     flag = 0
-        #     for j in range(n):
-        #         if i == nums[j]:
-        #             flag = 1
-        #             break
-        #     if flag == 0:
-        #         return i
+        # ss = n*(n+1)//2
+        # s = 0
+        # for i in range(n):
+        #     s += nums[i]
+        # return ss - s
+
+        n = len(nums)
+        for i in range(n+1):
+            flag = 0
+            for j in range(n):
+                if i == nums[j]:
+                    flag = 1
+                    break
+            if flag == 0:
+                return i
 
 
         

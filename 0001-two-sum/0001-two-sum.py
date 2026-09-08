ea6279512edff6a0024a47dd925038640a4f5129
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-09-08 23:39:49 +0530

    Time: 2 ms (56.25%), Space: 19.9 MB (66.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,14 +6,35 @@ class Solution:
         #             return [i, j]
         # return [-1, -1]
 
-        hash_map = {}
-        for i in range(len(nums)):
-            a = nums[i]
-            more = target - a
-            if more in hash_map:
-                return [hash_map[more], i]
-            hash_map[a] = i
+        # hash_map = {}
+        # for i in range(len(nums)):
+        #     a = nums[i]
+        #     more = target - a
+        #     if more in hash_map:
+        #         return [hash_map[more], i]
+        #     hash_map[a] = i
+        # return []
+
+        original_nums = nums
+        nums = sorted(nums)
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left < right:
+            summ = nums[left] + nums[right]
+            if summ == target:
+                idx1 = original_nums.index(nums[left])
+                if nums[left] == nums[right]:
+                    idx2 = original_nums.index(nums[right], idx1 + 1)
+                else:
+                    idx2 = original_nums.index(nums[right])
+                return [idx1, idx2]
+            elif summ < target:
+                left += 1
+            else:
+                right -= 1
         return []
+
 
 
         

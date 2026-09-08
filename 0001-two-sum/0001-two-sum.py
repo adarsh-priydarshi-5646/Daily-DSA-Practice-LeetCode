18fb3aa80e9c8e27585b8de949bc152186a2ff21
Adarsh Priydarshi <adarshpriydarshi5646@gmail.com>
2026-09-08 23:16:44 +0530

    Time: 1734 ms (20.17%), Space: 20.1 MB (65.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,38 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        original_nums = nums
-        nums = sorted(nums)
-        left = 0
-        right = len(nums) - 1
-        
-        while left < right:
-            summ = nums[left] + nums[right]
-            if summ == target:
-                idx1 = original_nums.index(nums[left])
-                if nums[left] == nums[right]:
-                    idx2 = original_nums.index(nums[right], idx1 + 1)
-                else:
-                    idx2 = original_nums.index(nums[right])
-                return [idx1, idx2]
-            elif summ < target:
-                left += 1
-            else:
-                right -= 1         
-        return []
-
-
-        # hash_map = {}
-        # for i in range(len(nums)):
-        #     a = nums[i]
-        #     more = target - a
-        #     if more in hash_map:
-        #         return [hash_map[more], i]
-        #     hash_map[a] = i
-        # return []
-
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return i,j
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return [-1, -1]
 
         
